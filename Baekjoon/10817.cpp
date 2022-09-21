4481fe05d112a6f911120c049821f85fb60c185b
gabjuho <gabjuho@gmail.com>
2022-09-21 22:10:02 +0900

    2022.09.21 문제풀이
    10817

---
+++ Baekjoon/10817.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	vector<int> v;
+	for (int i = 0; i < 3; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[1];
+}
