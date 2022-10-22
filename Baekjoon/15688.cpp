28899be89685fd4517eb43c29ce18e55baa5af13
gabjuho <gabjuho@gmail.com>
2022-10-22 23:27:31 +0900

    2022.10.22 문제풀이
    15688

---
+++ Baekjoon/15688.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool cmp(int a, int b) 
+{
+	return a < b;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	vector<int> v;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < n; i++)
+		cout << v[i] << '\n';
+}
