087f516f49922625f484d8777d4e5f5a5e402106
gabjuho <gabjuho@gmail.com>
2023-03-05 10:15:47 +0900

    2023.03.05 문제풀이(3)
    24266, 24267, 24313

---
+++ Baekjoon/24313.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a1, a0, c, n0;
+
+	cin >> a1 >> a0;
+	cin >> c;
+	cin >> n0;
+
+	if (a1 * n0 + a0 <= c * n0 && a1 <= c)
+		cout << 1;
+	else
+		cout << 0;
+}
