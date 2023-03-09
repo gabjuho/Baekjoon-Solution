20616374ce52c14b0f3945fced85b321eaa1720c
gabjuho <gabjuho@gmail.com>
2023-03-09 16:44:08 +0900

    2023.03.09 문제풀이
    6549

--- Baekjoon/6549.cpp
+++ Baekjoon/6549.cpp
@@ -2,27 +2,57 @@
 #include<stack>
 using namespace std;
 
+long long h[100001];
+
 int main()
 {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	while (1)
 	{
-		int num;
-		cin >> num;
+		stack<int> s;
+		int n;
+		long long max = -1;
+		cin >> n;
 
-		if (num == 0)
+		if (n == 0)
 			break;
 
-		stack<int> s;
-		for (int i = 0; i < num; i++)
+		for (int i = 0; i < n; i++)
+			cin >> h[i];
+
+		for (int i = 0; i < n; i++)
 		{
-			int height;
-			cin >> height;
-
-			
-			if (!s.empty() && height <= s.top())
+			while (!s.empty() && h[i] < h[s.top()])
 			{
+				long long hTop = h[s.top()];
+				int l = i;
+				s.pop();
 
+				if (!s.empty())
+					l = i - s.top() - 1;
+
+				if (max < l * hTop)
+					max = l * hTop;
 			}
+
+			s.push(i);
 		}
+
+		while (!s.empty())
+		{
+			long long hTop = h[s.top()];
+			int l = n;
+			s.pop();
+
+			if (!s.empty())
+				l = n - s.top() - 1;
+			if (max < l * hTop)
+				max = l * hTop;
+		}
+
+		cout << max << '\n';
 	}
 }
