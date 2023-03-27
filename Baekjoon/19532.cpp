8207826aa9c42729fb5beeede4f4538e26e0a557
gabjuho <gabjuho@gmail.com>
2023-03-27 23:30:10 +0900

    2023.03.27 문제풀이
    19532

---
+++ Baekjoon/19532.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int i = -999; i <= 999; i++)
+	{
+		for (int j = -999; j <= 999; j++)
+		{
+			if (a * i + b * j == c)
+			{
+				if (d * i + e * j == f)
+				{
+					cout << i << ' ' << j;
+					return 0;
+				}
+			}
+		}
+	}
+}
