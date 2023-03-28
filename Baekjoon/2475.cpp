f67af1037fa3da1f8f472b94ce893b4bd870bfe8
gabjuho <gabjuho@gmail.com>
2023-03-28 23:55:10 +0900

    2023.03.28 문제풀이
    2475

---
+++ Baekjoon/2475.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5];
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i] * arr[i];
+	}
+
+	cout << sum % 10;
+}
