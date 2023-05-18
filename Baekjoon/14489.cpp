e8a1b19a6dc96889fdaec02f5fffe420dcc5f403
gabjuho <gabjuho@gmail.com>
2023-05-18 13:52:04 +0900

    2023.05.18 문제풀이
    14489

---
+++ Baekjoon/14489.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+
+	cin >> a >> b >> c;
+
+	if (a + b >= c * 2)
+	{
+		cout << a + b - c * 2;
+	}
+	else
+	{
+		cout << a + b;
+	}
+}
