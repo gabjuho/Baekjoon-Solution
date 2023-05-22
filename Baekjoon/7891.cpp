39304ddc2a9b90711a2120e15026739b5e579db2
gabjuho <gabjuho@gmail.com>
2023-05-22 19:12:45 +0900

    2023.05.22 문제풀이
    7891

---
+++ Baekjoon/7891.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		int num1, num2;
+		cin >> num1 >> num2;
+
+		cout << num1 + num2 << '\n';
+	}
+}
